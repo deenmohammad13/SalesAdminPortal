9b5c885c77fca892f52baf08db13f5c2a8b8fdd0
Deen Mohammad <deen9721@gmail.com>
2024-10-19 14:51:45 -0700

    Creating the Entities

---
+++ SalesAdminPortalAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesAdminPortalAPI.Models.Entities;
+
+namespace SalesAdminPortalAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        // {
+
+        // }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+          
+        }
+        public DbSet<Salesman> Salesmans { get; set; }
+    }
+}
--- SalesAdminPortalAPI/obj/Debug/net8.0/SalesAdminPortalAPI.AssemblyInfo.cs
+++ SalesAdminPortalAPI/obj/Debug/net8.0/SalesAdminPortalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesAdminPortalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2314b48c6bbc6fc81f8f349ec20136c5285926")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720ebbdf7fa3df2faf95b121b7099a6ccb62bdfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesAdminPortalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesAdminPortalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
